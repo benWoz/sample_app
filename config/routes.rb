3c16d556e46e63c8da424e3d7945674290d67e10
Ben Woznicki <b@benWoz.com>
2011-02-23 17:33:31 -0500

    moved routes, added route tests & removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/contact"
+  root :to => 'pages#home'
 
-  get "pages/about"
-
-  get "pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
