07e2f85d5103cc12e1c1220f675428349876ad06
Ben Woznicki <b@benWoz.com>
2011-03-02 10:48:46 -0500

    Completed 'user validation' from 6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
 
-  validates :name, :presence => true
-  validates :email, :presence => true
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  :presence   => true,
+                    :length     => { :maximum => 50 }
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
